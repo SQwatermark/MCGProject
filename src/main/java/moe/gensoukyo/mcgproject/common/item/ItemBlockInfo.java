f5d1308076030a3879e44c0e336dad64baf43bb8
SQwatermark <sqwatermark@outlook.com>
2020-03-30 22:18:22 +0800

    加入了方块勘探仪，为魔豆（大橡树生成器）添加了材质

---
+++ src/main/java/moe/gensoukyo/mcgproject/common/item/ItemBlockInfo.java
@@ -0,0 +1,89 @@
+package moe.gensoukyo.mcgproject.common.item;
+
+import moe.gensoukyo.mcgproject.common.creativetab.MCGTabs;
+import moe.gensoukyo.mcgproject.common.util.MaterialHelper;
+import moe.gensoukyo.mcgproject.core.MCGProject;
+import net.minecraft.block.Block;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.Random;
+
+public class ItemBlockInfo extends Item {
+
+    public ItemBlockInfo() {
+        this.setMaxStackSize(1);
+        this.setCreativeTab(MCGTabs.FANTASY);
+        this.setRegistryName(MCGProject.ID, "block_info_displayer");
+        this.setTranslationKey(MCGProject.ID + "." + "block_info_displayer");
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack stack, @Nullable World world, List<String> list, ITooltipFlag flag)
+    {
+        list.add("右击显示方块信息");
+    }
+
+    @Override
+    public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+    {
+        IBlockState iblockstate = worldIn.getBlockState(pos);
+        Block block = iblockstate.getBlock();
+        Random random = new Random();
+        int colorIndex = random.nextInt(16);
+        TextFormatting color = TextFormatting.fromColorIndex(colorIndex);
+        StringBuilder s = new StringBuilder();
+        for (IProperty<?> iProperty : iblockstate.getProperties().keySet()) {
+            String key = iProperty.getName();
+            String value = iblockstate.getValue(iProperty).toString();
+            s.append(key).append("=").append(value);
+            s.append(",");
+        }
+        String s1;
+         s1 = s.length() > 0 ? s.substring(0, s.length()-1) : "无";
+
+        if (!worldIn.isRemote) {
+            player.sendMessage(new TextComponentString(color + block.getLocalizedName()));
+            player.sendMessage(new TextComponentString(color + "    SID: " + block.getRegistryName()));
+            player.sendMessage(new TextComponentString(color + "    NID: " + Block.getIdFromBlock(block) + ":" + block.getMetaFromState(iblockstate)));
+            player.sendMessage(new TextComponentString(color + "    方块状态: " + s1));
+            player.sendMessage(new TextComponentString(color
+                    + "    材料: " + MaterialHelper.getMaterial(iblockstate.getMaterial())
+                    + "    硬度: " + iblockstate.getBlockHardness(worldIn, pos)
+                    + "    亮度: " + block.getLightValue(iblockstate, worldIn, pos)
+                    + "    透光度: " + block.getLightOpacity(iblockstate, worldIn, pos)
+                    ));
+            player.sendMessage(new TextComponentString(color
+                    + "    渲染层: " + block.getRenderLayer()
+                    + "    渲染类型: " + iblockstate.getRenderType()
+                    + "    透明性: " + (iblockstate.isTranslucent() ? "半透明" : (iblockstate.isOpaqueCube() ? "不透明" : "透明"))
+            ));
+            player.sendMessage(new TextComponentString(color
+                    + "    是否造成窒息: " + iblockstate.causesSuffocation()
+                    + "    是否为方块实体: " + block.hasTileEntity()
+            ));
+
+        } else {
+            return EnumActionResult.PASS;
+        }
+
+        return EnumActionResult.SUCCESS;
+    }
+
+}
---
+++ src/main/java/moe/gensoukyo/mcgproject/common/util/MaterialHelper.java
@@ -0,0 +1,47 @@
+package moe.gensoukyo.mcgproject.common.util;
+
+import net.minecraft.block.material.Material;
+
+public class MaterialHelper {
+
+    public static String getMaterial(Material materialIn)
+    {
+        String s = "未知材料";
+        if (materialIn == Material.AIR) s = "空气";
+        else if (materialIn == Material.GRASS) s = "草";
+        else if (materialIn == Material.GROUND) s = "泥土";
+        else if (materialIn == Material.WOOD) s = "木头";
+        else if (materialIn == Material.ROCK) s = "岩石";
+        else if (materialIn == Material.IRON) s = "金属";
+        else if (materialIn == Material.ANVIL) s = "金属";
+        else if (materialIn == Material.WATER) s = "水";
+        else if (materialIn == Material.LAVA) s = "熔岩";
+        else if (materialIn == Material.LEAVES) s = "树叶";
+        else if (materialIn == Material.PLANTS) s = "植物";
+        else if (materialIn == Material.VINE) s = "藤蔓";
+        else if (materialIn == Material.SPONGE) s = "海绵";
+        else if (materialIn == Material.CLOTH) s = "布料";
+        else if (materialIn == Material.FIRE) s = "火";
+        else if (materialIn == Material.SAND) s = "沙子";
+        else if (materialIn == Material.CIRCUITS) s = "红石和石头";
+        else if (materialIn == Material.CARPET) s = "地毯";
+        else if (materialIn == Material.GLASS) s = "玻璃";
+        else if (materialIn == Material.REDSTONE_LIGHT) s = "草";
+        else if (materialIn == Material.TNT) s = "三硝基甲苯";
+        else if (materialIn == Material.CORAL) s = "不属于陆地上的植物";
+        else if (materialIn == Material.ICE) s = "冰";
+        else if (materialIn == Material.PACKED_ICE) s = "冰";
+        else if (materialIn == Material.SNOW) s = "雪";
+        else if (materialIn == Material.CRAFTED_SNOW) s = "雪";
+        else if (materialIn == Material.CACTUS) s = "草";
+        else if (materialIn == Material.CLAY) s = "黏土";
+        else if (materialIn == Material.GOURD) s = "瓜";
+        else if (materialIn == Material.DRAGON_EGG) s = "龙蛋";
+        else if (materialIn == Material.PORTAL) s = "草";
+        else if (materialIn == Material.CAKE) s = "蛋糕";
+        else if (materialIn == Material.WEB) s = "蛛丝";
+
+        return s;
+    }
+
+}
