2a73846bc4caae7d1c78ad7bdfd6e12393b22f34
SQwatermark <sqwatermark@outlook.com>
2020-04-23 16:20:07 +0800

    Update RenderMusicPlayer.java

--- src/main/java/moe/gensoukyo/mcgproject/cilent/entity/RenderMusicPlayer.java
+++ src/main/java/moe/gensoukyo/mcgproject/cilent/entity/RenderMusicPlayer.java
@@ -1,7 +1,6 @@
 package moe.gensoukyo.mcgproject.cilent.entity;
 
 import moe.gensoukyo.mcgproject.common.feature.musicplayer.EntityMusicPlayer;
-import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.RenderMinecart;
 import net.minecraftforge.fml.client.registry.IRenderFactory;
