32ea31b0ce3a6cc55bf5b9d5aa5b8c9f9c9b9685
SQwatermark <sqwatermark@outlook.com>
2020-05-29 17:31:41 +0800

    更改版本号

--- src/main/java/moe/gensoukyo/mcgproject/core/MCGProject.java
+++ src/main/java/moe/gensoukyo/mcgproject/core/MCGProject.java
@@ -31,7 +31,7 @@ public class MCGProject {
 
     public static final String ID = "mcgproject";
     public static final String NAME = "MCGProject";
-    public static final String VERSION = "1.1.9";
+    public static final String VERSION = "1.2.0";
 
     public static Logger logger;
     public static File modConfigDi;
