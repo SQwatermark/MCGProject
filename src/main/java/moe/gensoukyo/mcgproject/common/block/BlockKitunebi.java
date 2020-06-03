a01305d2a9ae3454c05b4875843f2941cf22dc97
drzzm32 <drzzm32@gmail.com>
2020-06-03 10:07:59 +0800

    fix again

--- src/main/java/moe/gensoukyo/mcgproject/common/block/BlockKitunebi.java
+++ src/main/java/moe/gensoukyo/mcgproject/common/block/BlockKitunebi.java
@@ -1,7 +1,6 @@
 package moe.gensoukyo.mcgproject.common.block;
 
 import moe.gensoukyo.mcgproject.common.creativetab.MCGTabs;
-import moe.gensoukyo.mcgproject.common.init.ModSound;
 import moe.gensoukyo.mcgproject.core.MCGProject;
 import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
@@ -12,14 +11,12 @@ import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
