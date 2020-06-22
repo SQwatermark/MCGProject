8449943542e1f08516690d96211614c9e8af9a3f
SQwatermark <sqwatermark@outlook.com>
2020-06-22 11:35:20 +0800

    变更版本号

--- src/main/java/moe/gensoukyo/mcgproject/core/MCGProject.java
+++ src/main/java/moe/gensoukyo/mcgproject/core/MCGProject.java
@@ -32,7 +32,7 @@ public class MCGProject {
 
     public static final String ID = "mcgproject";
     public static final String NAME = "MCGProject";
-    public static final String VERSION = "1.2.1";
+    public static final String VERSION = "1.2.3";
 
     public static Logger logger;
     public static File modConfigDi;
