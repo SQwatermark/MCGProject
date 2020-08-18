a1643b05dfda9e5f3078b3c50dcbcb33b240086b
SQwatermark <sqwatermark@outlook.com>
2020-08-18 11:09:16 +0800

    删除多余图片，缩小图片体积，修复低概率怪刷不出的bug

--- src/main/java/moe/gensoukyo/mcgproject/cilent/feature/SimpleAntiHack.java
+++ src/main/java/moe/gensoukyo/mcgproject/cilent/feature/SimpleAntiHack.java
@@ -6,6 +6,10 @@ import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
 
 @SideOnly(Side.CLIENT)
 public class SimpleAntiHack {
@@ -35,13 +39,24 @@ public class SimpleAntiHack {
             }
         } catch (Exception ignored) {
         }
+        try {
+            File file = new File(MCGProject.modDi, "Armourers-Workshop-1.12.2-0.50.5.jar");
+            if (Class.forName("moe.plushie.armourers_workshop.ArmourersWorkshop") != null) {
+                if (!file.exists()) {
+                    return "armors workshop name changed";
+                } else if (!DigestUtils.md5Hex(new FileInputStream(file.getPath())).equals("cea2c120c8cc823a6de83a002b8ca375")) {
+                    return  "armors workshop file changed";
+                }
+            }
+        } catch (Exception ignored) {
+        }
         return null;
     }
 
     public void preInit() {
         String hack = this.check();
         if (hack != null) {
-            MCGProject.logger.warn("AntiHack: 正在使用外挂[" + hack + "]");
+            MCGProject.logger.warn("AntiHack: 正在使用违规客户端[" + hack + "]");
             ShouterThread st = new ShouterThread("老大哥在看着你", "Cheat Client Detected");
             st.start();
             Minecraft.getMinecraft().shutdown();
